d140fdb2a920558d09defc8ff63ff6066d9b40ba
Kesav-21 <cgkesav@gmail.com>
2023-06-16 19:57:46 +0530

    Homepage and BottomNav updated

---
+++ lib/common/BottomNav.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatelessWidget {
+  const BottomNav({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          height: 50,
+          child: Row(
+            children: [
+              Container(
+                child: IconButton(
+                  icon: Icon(Icons.home),
+                  onPressed: () => {},
+                ),
+              ),
+              Container(
+                child: IconButton(
+                  icon: Icon(Icons.laptop_chromebook),
+                  onPressed: () => {},
+                ),
+
+              ),
+              Container(
+                child: IconButton(
+                  icon: Icon(Icons.school),
+                  onPressed: () => {},
+                ),
+              ),
+              Container(
+                child: IconButton(
+                  icon: Icon(Icons.stars_sharp),
+                  onPressed: () => {},
+                ),
+              ),
+              Container(
+                child: IconButton(
+                  icon: Icon(Icons.contact_page),
+                  onPressed: () => {},
+                ),
+              ),
+            ],
+          ),
+        );
+  }
+}
