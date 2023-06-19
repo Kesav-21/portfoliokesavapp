66ea1318fe909ae0810238e732f814151822d1ff
Kesav-21 <cgkesav@gmail.com>
2023-06-19 18:01:11 +0530

    Certifications Updated

--- lib/common/CertCard.dart
+++ lib/common/CertCard.dart
@@ -4,10 +4,18 @@ class CertCard extends StatelessWidget {
   CertCard({super.key});
 
   final List<MyCertifications> myCertifications=[
+    MyCertifications("Crash Course on Python","assets/images/K.jpg","by Google\nJun 2023",["Python"]),
+    MyCertifications("Using Python to Interact with the operating System","assets/images/K.jpg","by Google\nJun 2023",["Python","Bash","Linux"]),
+    MyCertifications("Web Development with Python and Flask","assets/images/K.jpg","by Jovian\nMar 2023",["Python","Flask","HTML","CSS"]),
+    MyCertifications("Learning Java","assets/images/K.jpg","by Linkedin\nDec 2022",["Java"]),
+    MyCertifications("Front-End Development with React","assets/images/K.jpg","by The HongKong University\nNov 2022",["HTML","CSS","ReactJS","Bootstrap"]),
+    MyCertifications("Python for Data Science, AI & Development","assets/images/K.jpg","by IBM\nFeb 2022",["Python","Numpy","Pandas"]),
+    MyCertifications("Tools for Data Science","assets/images/K.jpg","by IBM\nJan 2022",["Python","WebScraping"]),
+    MyCertifications("Front-End Development Libraries","assets/images/K.jpg","by FreeCodeCamp\nOct 2021",["ReactJS","Bootstrap","Redux"]),
+    MyCertifications("JavaScript Algorithms and Data Structures","assets/images/K.jpg","by FreeCodeCamp\nMay 2021",["Javascript"]),
     MyCertifications("Responsive Web Design","assets/images/K.jpg","by FreeCodeCamp\nMay 2021",["HTML","CSS"]),
-    MyCertifications("JavaScript Algorithms and Data Structures","assets/images/K.jpg","by FreeCodeCamp\nMay 2021",["HTML","CSS"]),
-    MyCertifications("Front-End Development Libraries","assets/images/K.jpg","by FreeCodeCamp\nMay 2021",["HTML","CSS"]),
-    MyCertifications("Python for Data Science, AI & Development","assets/images/K.jpg","by FreeCodeCamp\nMay 2021",["HTML","CSS"]),
+    MyCertifications("Python","assets/images/K.jpg","by GUVI\nNov 2022",["Python"]),
+    MyCertifications("Data Analysis Zero to Pandas","assets/images/K.jpg","by Jovian\nNov 2022",["Python","Numpy","Pandas"]),
   ];
 
 
@@ -55,3 +63,4 @@ class MyCertifications {
 
   MyCertifications(this.tname, this.image, this.desc, this.tech);
 }
+
