0bf085dc58254a7d182e60d8b4a2c037cda89fe1
Kesav-21 <cgkesav@gmail.com>
2023-06-20 12:22:06 +0530

    Certifications assets updated

--- lib/common/CertCard.dart
+++ lib/common/CertCard.dart
@@ -4,18 +4,18 @@ class CertCard extends StatelessWidget {
   CertCard({super.key});
 
   final List<MyCertifications> myCertifications=[
-    MyCertifications("Crash Course on Python","assets/images/K.jpg","by Google\nJun 2023",["Python"]),
-    MyCertifications("Using Python to Interact with the operating System","assets/images/K.jpg","by Google\nJun 2023",["Python","Bash","Linux"]),
-    MyCertifications("Web Development with Python and Flask","assets/images/K.jpg","by Jovian\nMar 2023",["Python","Flask","HTML","CSS"]),
-    MyCertifications("Learning Java","assets/images/K.jpg","by Linkedin\nDec 2022",["Java"]),
-    MyCertifications("Front-End Development with React","assets/images/K.jpg","by The HongKong University\nNov 2022",["HTML","CSS","ReactJS","Bootstrap"]),
-    MyCertifications("Python for Data Science, AI & Development","assets/images/K.jpg","by IBM\nFeb 2022",["Python","Numpy","Pandas"]),
-    MyCertifications("Tools for Data Science","assets/images/K.jpg","by IBM\nJan 2022",["Python","WebScraping"]),
-    MyCertifications("Front-End Development Libraries","assets/images/K.jpg","by FreeCodeCamp\nOct 2021",["ReactJS","Bootstrap","Redux"]),
-    MyCertifications("JavaScript Algorithms and Data Structures","assets/images/K.jpg","by FreeCodeCamp\nMay 2021",["Javascript"]),
-    MyCertifications("Responsive Web Design","assets/images/K.jpg","by FreeCodeCamp\nMay 2021",["HTML","CSS"]),
-    MyCertifications("Python","assets/images/K.jpg","by GUVI\nNov 2022",["Python"]),
-    MyCertifications("Data Analysis Zero to Pandas","assets/images/K.jpg","by Jovian\nNov 2022",["Python","Numpy","Pandas"]),
+    MyCertifications("Crash Course on Python","assets/images/google.png","by Google\nJun 2023",["Python"]),
+    MyCertifications("Using Python to Interact with the operating System","assets/images/google.png","by Google\nJun 2023",["Python","Bash","Linux"]),
+    MyCertifications("Web Development with Python and Flask","assets/images/jovian.png","by Jovian\nMar 2023",["Python","Flask","HTML","CSS"]),
+    MyCertifications("Learning Java","assets/images/linkedin.png","by Linkedin\nDec 2022",["Java"]),
+    MyCertifications("Front-End Development with React","assets/images/hkust.png","by The HongKong University\nNov 2022",["HTML","CSS","ReactJS","Bootstrap"]),
+    MyCertifications("Python for Data Science, AI & Development","assets/images/ibm.png","by IBM\nFeb 2022",["Python","Numpy","Pandas"]),
+    MyCertifications("Tools for Data Science","assets/images/ibm.png","by IBM\nJan 2022",["Python","WebScraping"]),
+    MyCertifications("Front-End Development Libraries","assets/images/fcc.png","by FreeCodeCamp\nOct 2021",["ReactJS","Bootstrap","Redux"]),
+    MyCertifications("JavaScript Algorithms and Data Structures","assets/images/fcc.png","by FreeCodeCamp\nMay 2021",["Javascript"]),
+    MyCertifications("Responsive Web Design","assets/images/fcc.png","by FreeCodeCamp\nMay 2021",["HTML","CSS"]),
+    MyCertifications("Python","assets/images/guvi.png","by GUVI\nNov 2022",["Python"]),
+    MyCertifications("Data Analysis Zero to Pandas","assets/images/jovian.png","by Jovian\nNov 2022",["Python","Numpy","Pandas"]),
   ];
 
 
@@ -30,7 +30,7 @@ class CertCard extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 ListTile(
-                  trailing: Image.asset('assets/images/K.jpg'),
+                  trailing: Image.asset(edu.image),
                   title: Text(edu.tname),
                   subtitle: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
